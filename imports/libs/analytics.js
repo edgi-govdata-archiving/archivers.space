494de7010053d4b8e23eab4493d8f517e8c9c3e5
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-03-28 14:38:10 -0400

    experimenting with elevating analytics.js script to html scope

--- imports/libs/analytics.js
+++ imports/libs/analytics.js
@@ -3,9 +3,7 @@ import { Meteor } from 'meteor/meteor';
 let analytics = [];
 
 if (Meteor.isClient) {
-  analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
-  analytics.load('FwUGnmKzryJpDpApVzdQy9rmwwWSiK1M');
-  }
+  analytics = window.analytics;
 } else {
   // falsify analytics on the server so calls don't explode things
   analytics.methods = [
